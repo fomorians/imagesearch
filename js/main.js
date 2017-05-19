720310d0a1b857afc2b9090961c5dbc4f499f80b
Danielle Swank <danielle@danielleswank.com>
2017-05-18 17:25:33 -0700

    updated to work with the new api

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 (function() {
   const API_ENDPOINT = '//fomoro-website.appspot.com/api/related-images';
-  const IMAGE_ROOT = '//storage.googleapis.com/fomoro-website-related-images/images';
+  const IMAGE_ROOT = '//storage.googleapis.com/fomoro-website-related-images/thumbs';
   const MAX_RANGE = 1000;
 
   function getRandomInt(min, max) {
@@ -8,32 +8,30 @@
   }
 
   function fetchRelated(image_id) {
-    const url = `${API_ENDPOINT}/${image_id}`
+    const url = `${API_ENDPOINT}/${image_id}/sets/3`
     return fetch(url).then(res => res.json());
   }
 
   class App {
     constructor() {
       this.first_el = document.getElementById('first');
-      this.randomize_el = document.getElementById('randomize');
       this.result_els = document.getElementsByClassName('result');
+      this.randomize_el = document.getElementById('randomize');
 
       this.randomize_el.setAttribute("data-id", getRandomInt(1, MAX_RANGE));
       this.randomize_el.addEventListener('click', this.getImages.bind(this, this.randomize_el));
-      window.addEventListener('popstate', this.getPrevState.bind(this));
 
-      _.each(this.result_els, (result_el, i) => {
-        result_el.addEventListener('click', this.getImages.bind(this, result_el));
+      _.each(document.querySelectorAll('section a'), (el, i) => {
+        el.addEventListener('click', this.getImages.bind(this, el));
       })
+
+      window.addEventListener('popstate', this.getPrevState.bind(this));
     }
 
     update(results) {
-      const first = results.shift()
-      this.first_el.setAttribute("src", `${IMAGE_ROOT}/${first.image}`);
       this.randomize_el.setAttribute("data-id", getRandomInt(1, MAX_RANGE));
-
-      _.each(results, (result, i) => {
-        const result_el = this.result_els[i];
+      _.each(_.flatten(results), (result, i) => {
+        let result_el = this.result_els[i];
         result_el.setAttribute("data-id", result.id);
         result_el.setAttribute("src", `${IMAGE_ROOT}/${result.image}`);
       });
@@ -46,16 +44,20 @@
 
     getImages(el, e) {
       e.preventDefault();
-      const image_id = el.getAttribute("data-id");
+      let image_id = el.getAttribute("data-id");
+      if (!image_id) {
+        let img_el = el.getElementsByTagName('img')[0];
+        image_id = img_el.getAttribute("data-id");
+      }
+
       fetchRelated(image_id).then(data => {
-        history.pushState({'images':data}, null, `/imagesearch/#/${image_id}`);
+        history.pushState({'images': data}, null, `${HTML_ENDPOINT}/${image_id}`);
         this.update(data);
       });
     }
 
     start() {
       let image_id = parseInt(window.location.hash.split('/').pop());
-
       if (Number.isNaN(image_id) ||image_id < 0 || image_id > MAX_RANGE) {
         image_id = getRandomInt(1, MAX_RANGE);
       }
